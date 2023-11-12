2efc3c1854170a087f1302c972bcd6ff66a38898
IntepeZafer <intepezafer@gmail.com>
2023-11-13 00:17:13 +0300

    Multiple Screen Commit

---
+++ MultipleScreen/app/src/main/java/com/zafer/multiplescreen/secondActivity.java
@@ -0,0 +1,20 @@
+package com.zafer.multiplescreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class secondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+    public void changeToScreen(View view){
+        Intent intent = new Intent(secondActivity.this , MainActivity.class);
+        startActivity(intent);
+    }
+}
