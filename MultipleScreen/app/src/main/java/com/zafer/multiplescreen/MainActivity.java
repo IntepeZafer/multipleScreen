afc3bfe16e25535fe0028597d3f1f8bdea8d9268
IntepeZafer <intepezafer@gmail.com>
2023-11-13 22:50:15 +0300

    Multiple App Compalte

--- MultipleScreen/app/src/main/java/com/zafer/multiplescreen/MainActivity.java
+++ MultipleScreen/app/src/main/java/com/zafer/multiplescreen/MainActivity.java
@@ -5,16 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editTextText);
     }
     public void changeToScreen(View view){
+        username = editText.getText().toString();
         Intent intent = new Intent(MainActivity.this , secondActivity.class);
+        intent.putExtra("usernameInput" , username);
         startActivity(intent);
+
     }
 }
--- MultipleScreen/app/src/main/java/com/zafer/multiplescreen/secondActivity.java
+++ MultipleScreen/app/src/main/java/com/zafer/multiplescreen/secondActivity.java
@@ -5,13 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class secondActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        textView = findViewById(R.id.textView2);
+        Intent intent = getIntent();
+        String unerName = intent.getStringExtra("usernameInput");
+        textView.setText(unerName);
     }
     public void changeToScreen(View view){
         Intent intent = new Intent(secondActivity.this , MainActivity.class);
